14a6d318efec9d6704d827dcfc64c7d5f2cf4fa6
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-08 07:07:28 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+apt install net-tools
+ip='ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"'
+mkdir keys
 cntFile=".showcnt.txt"
 if [ ! -f $cntFile ]; then
 echo "未运行step1！"
@@ -6,9 +9,14 @@ exit
 fi
 
 tCnt=`cat $cntFile`
+
 for ((i=1; i<=tCnt; i ++))
 do
-bee start   \
---config /etc/bee/bee${i}.yaml
+cp /var/lib/node${tCnt}/keys/swarm.key node${tCnt} /keys/${ip}-${tCnt}.key
+screen_name=$"i"
+screen -dmS $screen_name
+cmd=$"bee start  --config node${i}.yaml";
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
 done
-sz /var/lib/bee/keys${tCnt}/swarm.key /var/lib/bee/password
+sz /keys/*
