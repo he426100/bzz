dc47c3bee787a05bcb7929fbce06ec0694295070
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-08 02:58:52 +0800

    Create step2.sh

---
+++ step2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cntFile=".showcnt.txt"
+if [ ! -f $cntFile ]; then
+echo "未运行step1！"
+exit
+fi
+
+tCnt=`cat $cntFile`
+for ((i=1; i<=tCnt; i ++))
+do
+bee start   \
+--config /etc/bee/bee${i}.yaml
+done
+sz /var/lib/bee/keys/swarm.key /var/lib/bee/password/*
