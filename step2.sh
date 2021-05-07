368f4b7aac1bc8d9ee939150d10343f03941a26a
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-08 07:46:44 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -14,8 +14,7 @@ do
 cp /var/lib/node${tCnt}/keys/swarm.key node${tCnt} /keys/${ip}-${tCnt}.key
 echo "00 02 * * * /root/cashout${i}.sh cashout-all" >> /etc/crontab
 screen -dmS bee$i
-cmd=$"bee start  --config node${i}.yaml";
-screen -x -S bee$i -p 0 -X stuff "$cmd"
+screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已启动！"
 echo "下载密钥至本地……"
