6927aaf496de138044424544c5102b9bdc89d185
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-08 09:11:59 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -12,7 +12,7 @@ for ((i=1; i<=tCnt; i ++))
 do
 echo "对第$i个节点添加自动提取。"
 cp /var/lib/node${i}/keys/swarm.key ./keys
-rn ./keys/swarm.key ./keys/swarm.key/${ip}-${i}.key
+mv ./keys/swarm.key ./keys/swarm.key/${ip}-${i}.key
 echo "00 02 * * * /root/cashout${i}.sh cashout-all" >> /etc/crontab
 screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
@@ -22,6 +22,6 @@ screen -ls
 done
 echo "下载密钥至本地……"
 cp /var/lib/bee/password ./keys
-rn ./keys/password ./keys/${ip}-password.txt
+mv ./keys/password ./keys/${ip}-password.txt
 sz /keys/*
 rm /keys/*
