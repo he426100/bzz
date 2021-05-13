87303fba4f0af5578b177b86896101981d5c19a8
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-13 15:29:42 +0800

    Update open_port.sh

--- open_port.sh
+++ open_port.sh
@@ -10,6 +10,6 @@ screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已重启。"
-screen -ls
 done
 sed -i '/cashout/d' /etc/crontab
+screen -ls
