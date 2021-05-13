097b48103acd05ccb03e1552faa41650f7f8da28
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-13 15:57:14 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -36,8 +36,8 @@ api-addr: :$((1534+${tCnt}))
 config: /root/node${tCnt}.yaml
 data-dir: /var/lib/bee/node${tCnt}
 db-capacity: 15000000
-debug-api-addr: :$((1634+${tCnt}))
-#debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
+#debug-api-addr: :$((1634+${tCnt}))
+debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
 debug-api-enable: true
 p2p-addr: :$((1734+${tCnt}))
 password-file: /var/lib/bee/password
