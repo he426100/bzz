208f36e4c94b01c3eb576e9a7343aa52e8536172
heyiming <1090195622@qq.com>
2021-06-14 01:22:11 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -11,11 +11,11 @@ for ((i=1; i<=tCnt; i ++))
 do
 echo "对第$i个节点添加自动提取。"
 echo "00 02 * * * root /data/bees/cashout${i}.sh cashout-all" >> /etc/crontab
-screen -dmS bee$i
-screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
-screen -x -S bee$i -p 0 -X stuff $'\n'
-echo "第$i个节点已启动。"
-sleep 2
+# screen -dmS bee$i
+# screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
+# screen -x -S bee$i -p 0 -X stuff $'\n'
+# echo "第$i个节点已启动。"
+# sleep 2
 done
 screen -ls
 echo "尝试添加监听脚本……"
